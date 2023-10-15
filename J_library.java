c143ea70c5aba3e41f726528eeec8f81cb66b197
chandrakant <vk401426@gmail.com>
2023-10-15 08:57:59 +0530

    output added

--- J_library.java
+++ J_library.java
@@ -4,5 +4,18 @@ author, and ISBN. Implement constructors to initialize book details.
  */
 
 public class J_library {
-    
+    String title,author,ISBN;
+    J_library()
+    {
+        System.out.println(" Enter book title,Author , ISBN  respectively \n");
+        {
+            
+        }
+    }
+    void view()
+    {
+        System.out.println("Title :"+title);
+        System.out.println("Author : "+author);
+        System.out.println("ISBN : "+ISBN);
+    }
 }
