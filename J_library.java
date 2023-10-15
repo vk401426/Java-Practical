d60650302f1f876457afd2cb870f2533a6e5fa0d
chandrakant <vk401426@gmail.com>
2023-10-15 09:24:25 +0530

    program J_library completed

--- J_library.java
+++ J_library.java
@@ -3,19 +3,38 @@ Create a program that models a library's book catalog. Define a Book class with 
 author, and ISBN. Implement constructors to initialize book details.
  */
 
-public class J_library {
+class book {
     String title,author,ISBN;
-    J_library()
+    book(String t,String a,String i,int n)
     {
-        System.out.println(" Enter book title,Author , ISBN  respectively \n");
-        {
-            
-        }
+        title=t;
+        author=a;
+        ISBN=i;
+        System.out.println(" Book initialised :  B"+1);
     }
     void view()
     {
         System.out.println("Title :"+title);
         System.out.println("Author : "+author);
         System.out.println("ISBN : "+ISBN);
+        System.out.println("\n--------------------------------");
+        
+    }
+}
+
+class J_library{
+    public static void main (String []args) 
+    {
+        book b1 =new book("let us c", "Yashwant Kanetkar", "KB91473",1);
+        book b2 = new book("Mathematics ","R .S Agarwal","HG72382",2);
+        book b3 = new book("Calculus ","R.D Sharma","JS5298382",3);
+        book b4 = new book("Biology ","J K Hassan","US26382",4);
+
+        System.out.println("\n\n**** Book Catelog ****");
+        b1.view();
+
+        b2.view();
+        b3.view();
+        b4.view();
     }
 }
